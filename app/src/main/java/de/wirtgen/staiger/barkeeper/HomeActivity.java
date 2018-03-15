9aae1cf7f7f888c66edb4b8ccd48e09d7fa1bb28
VivoUtMori <julian.m.staiger@student.hs-rm.de>
2018-03-15 22:27:00 +0100

    Menu Beschriftung angepasst

--- app/src/main/java/de/wirtgen/staiger/barkeeper/HomeActivity.java
+++ app/src/main/java/de/wirtgen/staiger/barkeeper/HomeActivity.java
@@ -29,22 +29,6 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (view.isSelected()) {
-                    view.setSelected(false);
-                    Snackbar.make(view, "Aus Favoriten entfernt", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                } else {
-                    view.setSelected(true);
-                    Snackbar.make(view, "Zu Favoriten hinzugefügt", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -100,27 +84,21 @@ public class HomeActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_ingredients) {
+
+        } else if (id == R.id.nav_settings) {
 
         }
 
