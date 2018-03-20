4aceea3841f46ccd91932544dd42022b0f951550
VivoUtMori <julian.m.staiger@student.hs-rm.de>
2018-03-20 11:29:53 +0100

    Fav Button lässt sich an und abwählen

--- app/src/main/java/de/wirtgen/staiger/barkeeper/HomeActivity.java
+++ app/src/main/java/de/wirtgen/staiger/barkeeper/HomeActivity.java
@@ -1,11 +1,7 @@
 package de.wirtgen.staiger.barkeeper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,8 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import org.greenrobot.greendao.database.Database;
 
 import java.util.List;
 import java.util.Map;
@@ -28,34 +22,34 @@ public class HomeActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setTitle("Home");
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         DaoSession daoSession = ((App) getApplication()).getDaoSession();
         CocktailDao cocktailDao = daoSession.getCocktailDao();
         List<Cocktail> listOfCocktails = cocktailDao.queryBuilder().where(CocktailDao.Properties.Name.like("%Long%")).list();
-        for (Cocktail c : listOfCocktails){
-            Log.d("HomeAktivity", "Loaded Cocktail from DB" + c.getName());
+        for (Cocktail c : listOfCocktails) {
+            Log.d("HomeActivity", "Loaded Cocktail from DB" + c.getName());
             Map<Ingredient, Integer> m = c.getAllIngredientsWithUnits();
-            Log.d("HomeAktivity", "Ingredients for cocktail: " + c.getName());
-            for (Map.Entry<Ingredient, Integer> e : m.entrySet()){
-                Log.d("HomeAktivity", "Entry: " + e.getKey().getName() + ": " + e.getValue().toString());
+            Log.d("HomeActivity", "Ingredients for cocktail: " + c.getName());
+            for (Map.Entry<Ingredient, Integer> e : m.entrySet()) {
+                Log.d("HomeActivity", "Entry: " + e.getKey().getName() + ": " + e.getValue().toString());
             }
         }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -65,17 +59,25 @@ public class HomeActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        switch (id) {
+            case R.id.button_favorite:
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                    item.setIcon(getResources().getDrawable(R.drawable.button_favorite_deactivated));
+                } else {
+                    item.setChecked(true);
+                    item.setIcon(getResources().getDrawable(R.drawable.button_favorite_activated));
+                }
+                break;
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -87,17 +89,20 @@ public class HomeActivity extends AppCompatActivity
 
         setTitle(item.getTitle());
 
-        if (id == R.id.nav_home) {
-
-        } else if (id == R.id.nav_search) {
-
-        } else if (id == R.id.nav_ingredients) {
-
-        } else if (id == R.id.nav_settings) {
-
+        switch (id) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_search:
+                break;
+            case R.id.nav_ingredients:
+                break;
+            case R.id.nav_favorites:
+                break;
+            case R.id.nav_settings:
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
